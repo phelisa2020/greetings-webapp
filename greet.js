fcf5561e75acf5f0194a7b76ffc3ce54b64beec2
Your Name <phelisagqwetha@gmail.com>
2020-10-08 17:26:52 +0200

    COMM

--- greet.js
+++ greet.js
@@ -1,73 +1,91 @@
 
-module.exports = function greet(){
-const namesGreeted = {}
+module.exports = function greet(pool) {
+	const namesGreeted = {}
 
-function storeName(userName){
-    let results = await pool.query('select greeted_name from greetings where greeted_name = $1', [userName]);
-	if (results.rowCount === 0){
-		await pool.query('insert into greetings (greeted_name, greeted_counter) value($1, $2)' , [userName ,1]);
-  
-}
+	async function storeName(userName) {
 
-await pool.query('update greetings set greeted_counter = greeted_counter +1  where greeted_name = $1',[userName]) 
-}
+		const SQLcheck = "select count(*) from greetings where greeted_name = $1";
+		const results = await pool.query(SQLcheck, [userName]);
+		console.log(results.rows);
 
-function counter (){
-	return Object.keys(namesGreeted).length
+		if(results.rows[0].count === 0){
+			const SQLinsert = "insert into greetings(greeted_name, greeted_count) values ($1, 1)"
+		return	await pool.query(SQLinsert, [userName])
+		}
 
-}
+		else if (results.rows.length > 0) {
+			const updateSQL = "upadate greetings set greeted_count = greeted_count + 1 where greeted_name = $1";
+		return	await pool.query(updateSQL, userName)
 
-
-function greeted(languagePicked, userName){
-	if (languagePicked && userName) {
-		// req.flash('info', 'please enter name!!!!!')
-		if(languagePicked === 'English'){
-
-			return ('Hi, '+ userName);
-}
- 	else if(languagePicked === 'Afrikaans'){
- 		return ('More, '+ userName);
- 	}
-
- 	else if(languagePicked === 'IsiXhosa'){
- 		return ('Molo, '+ userName);
- 	}
-	}
- 	
+			
+		}
+		
 	}
 
-	function errorMessages(languagePicked, userName){
-		var message = '';
-		if(userName === ""){
- message = "Please enter a name"; 
-  	}
-
-  	else if(!languagePicked){
- message =  "Please select language";
- 
- 
-  	}
-  	return message;
+	async function counter() {
+		let names = await pool.query ("select * from greetings");
+     return names.rowCount
 	}
-function getNames(){
-	return namesGreeted
-}
-function getNameCounter(userName){
-	// console.log({userName, namesGreeted});
-	// console.log(namesGreeted[userName]);
 
-	return  namesGreeted[userName] ;
-}
 
-return {
-	storeName,
-	counter,
-	greeted,
-	getNames,
-	errorMessages,
-	getNameCounter
+	async function greeted(languagePicked, userName) {
+		if (languagePicked && userName) {
+			// req.flash('info', 'please enter name!!!!!')
+			if (languagePicked === 'English') {
 
+				return ('Hi, ' + userName);
+			}
+			else if (languagePicked === 'Afrikaans') {
+				return ('More, ' + userName);
+			}
+
+			else if (languagePicked === 'IsiXhosa') {
+				return ('Molo, ' + userName);
+			}
+
+		}
+	}
+
+		async function errorMessages(languagePicked, userName) {
+			var message = '';
+			if (userName === "") {
+				message = "Please enter a name";
+			}
+
+			else if (!languagePicked) {
+				message = "Please select language"
+			}
+			return message;
+		}
+
+		async function getNames() {
+			//namesGreeted[userName] = 0;
+			const sql = "select * from greetings";
+			const results = await pool.query(sql);
+
+			const objectUser = {};
+			results.rows.forEach(function (user) {
+				objectUser[user.greeted_name] = user.greeted_count
+			})
+        	return objectUser
+		}
 	
+	async function getNameCounter(userName) {
+		// console.log({userName, namesGreeted});
+		// console.log(namesGreeted[userName]);
 
-}
+		return namesGreeted[userName];
+	}
+
+	return {
+		storeName,
+		counter,
+		greeted,
+		getNames,
+		errorMessages,
+		getNameCounter
+
+
+
+	}
 }
--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 const express = require("express");
-const exphbs  = require('express-handlebars');
+const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const greetings = require('./greet');
- const flash = require('express-flash');
- const session = require('express-session');
+const flash = require('express-flash');
+const session = require('express-session');
 const pg = require("pg");
 const Pool = pg.Pool;
- 
+
 
 // should we use a SSL connection
 let useSSL = false;
 let local = process.env.LOCAL || false;
-if (process.env.DATABASE_URL && !local){
-    useSSL = true;
+if (process.env.DATABASE_URL && !local) {
+  useSSL = true;
 }
-// which db connection to use
-const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/my_products';
+//` which db connection to use
+const connectionString = process.env.DATABASE_URL || 'postgresql://coder:pg123@localhost:5432/greetings';
 
 const pool = new Pool({
-    connectionString,
-    ssl : useSSL
-  });
-const greet = greetings() 
+  connectionString,
+  ssl: useSSL
+});
+const greet = greetings(pool)
 const app = express()
 
 
@@ -29,105 +29,106 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false })); // add this line
 app.use(bodyParser.json()); // add  this line
 
-app.engine('handlebars', exphbs({layoutsDir: './views/layouts'}));
+app.engine('handlebars', exphbs({ layoutsDir: './views/layouts' }));
 app.set('view engine', 'handlebars');
 
- app.use(session({
-     secret: 'This is my warning message',
-     resave: false,
-     saveUninitialized: true
-   }));
+app.use(session({
+  secret: 'This is my warning message',
+  resave: false,
+  saveUninitialized: true
+}));
 
-  // initialise the flash middleware
-   app.use(flash());
+// initialise the flash middleware
+app.use(flash());
 
 app.get('/addFlash', function (req, res) {
-    req.flash('info', 'Flash Message Added');
-    res.redirect('/');
-  });
+  req.flash('info', 'Flash Message Added');
+  res.redirect('/');
+});
 
-	app.get('/', function (req, res) {
+app.get('/', async function (req, res) {
 
-     res.render('index', {
+  res.render('index', {
     count: greet.counter()
- });
- });
- 
-
-	app.post('/greet', function(req, res){
-	 // req.flash('info', 'please enter name');
-  //    res.redirect('/');
-  let name = (req.body.greeted_name).toLowerCase();
-  let language = req.body.languageType;
-if (name === ''){
-  req.flash('info', 'please enter name'),
-  await pool.query('insert into greetings (greeted_name, greeted_counter) value($1, $2)' , [name ,language 1]);
-}
-else if(language === undefined && name != ''){
-   req.flash('info', 'please select a language'),
-   await pool.query('insert into greetings (greeted_name, greeted_counter) value($1, $2)' , [name ,language, 1]);
-
-}
-else{
-  greet.storeName(name, language),
-  await pool.query('insert into greetings (greeted_name, greeted_counter) value($1, $2)' , [name ,language 1]);
-
-	}	
-
-	// var error = greet.errorMessages(name, language);
- res.render('index',{
- 	message : greet.greeted(req.body.languageType , req.body.nameValue),
-  // message : (error === '') ? greet.greeted(req.body.languageType, req.body.nameValue): error,
-   count : greet.counter(),
-   
- })	
- });
-
-//#counter
- app.get('/greeted',function(req, res){
-var greetedNames = greet.getNames();
-for(const list in greetedNames){
-
-}
- 	res.render('greeted',{
- 	names : greetedNames
-
- 	})
- 	
- 	})
-
-
-app.get('/greeted/:userName', function(req, res){
-	const userName = req.params.userName;
-	var count = greet.getNameCounter(userName);
-	
-	
-	//it redirect the default route
-	res.render("greeted", {
-        name : `Hello, ${userName} have been greeted ${count} times`
-    });
-	
-	
+  });
 });
 
 
-  
-
-   
- app.get('/', function (req, res) {
-     req.flash('info', 'please enter name');
-     res.redirect('/');
-   });
- // app.get('/', function(req, res){
- //   // Get an array of flash messages by passing the key to req.flash()
- //   res.render('index', { messages: req.flash('info') });
- // });
-  
+// 	app.post('/greet', function(req, res){
+// 	 // req.flash('info', 'please enter name');
 
 
+app.post('/greet', async function (req, res) {
+  // req.flash('info', 'please enter name');
+  //    res.redirect('/');
+  let name = (req.body.nameValue).toLowerCase();
+  let language = req.body.languageType;
+  if (name === '') {
+    req.flash('info', 'please enter name')
+  }
+  else if (language === undefined && name != '') {
+    req.flash('info', 'please select a language')
+  }
+  else {
+    greet.storeName(name, language)
+  }
+
+  // var error = greet.errorMessages(name, language);
+  res.render('index', {
+    message: await greet.greeted(req.body.languageType, req.body.nameValue),
+    // message : (error === '') ? greet.greeted(req.body.languageType, req.body.nameValue): error,
+    count: await greet.counter()
+
+  })
+});
 
 
-const PORT = process.env.PORT || 3015;
-app.listen(PORT, function(){
-	console.log('App started at port:', PORT);
+
+//#counter
+app.get('/greeted', async function (req, res) {
+  var greetedNames = await greet.getNames();
+  for (const list in greetedNames) {
+
+  }
+  res.render('greeted', {
+    names: greetedNames
+
+  })
+
+})
+
+
+app.get('/greeted/:userName', async function (req, res) {
+  const userName = req.params.userName;
+  var count = await greet.getNameCounter(userName);
+
+
+  //it redirect the default route
+  res.render("greeted", {
+    name: `Hello, ${userName} have been greeted ${count} times`
+  });
+
+
+});
+
+
+
+
+
+app.get('/', async function (req, res) {
+  req.flash('info', 'please enter name');
+  res.redirect('/');
+});
+// app.get('/', function(req, res){
+//   // Get an array of flash messages by passing the key to req.flash()
+//   res.render('index', { messages: req.flash('info') });
+// });
+
+
+
+
+
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, function () {
+  console.log('App started at port:', PORT);
 })
